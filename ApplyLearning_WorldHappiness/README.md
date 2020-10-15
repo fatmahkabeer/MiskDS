cf79134ed06e81000981abd9494809f204b1eee2
fatmahkabeer <fatma.kabeer@hotmail.com>
2020-10-15 21:33:13 +0300

    with table of contact.

---
+++ ApplyLearning_WorldHappiness/README.md
@@ -0,0 +1,2 @@
+# WorldHappiness
+I have collected this data from Kaggle. It mainly represents the world happiness reports published from 2015 to 2019. I manipulate it to practice what I have learned from Rick Scavetta at Misk Academy. 
---
+++ ApplyLearning_WorldHappiness/happiness.R
@@ -0,0 +1,118 @@
+library(tidyverse)
+
+
+
+happiness_2015 <- read.csv("ApplyLearning_WorldHappiness/archive/2015.csv")
+happiness_2016 <- read.csv("ApplyLearning_WorldHappiness/archive/2016.csv")
+happiness_2017 <- read.csv("ApplyLearning_WorldHappiness/archive/2017.csv")
+happiness_2018 <- read.csv("ApplyLearning_WorldHappiness/archive/2018.csv")
+happiness_2019 <- read.csv("ApplyLearning_WorldHappiness/archive/2019.csv")
+
+#column names in each dataset
+names(happiness_2015)
+
+names(happiness_2016)
+names(happiness_2017)
+names(happiness_2018)
+names(happiness_2019)
+
+
+#Plot to explain the relationship between freedom to make life choices with the level of happiness.
+#How freedom affect countries' happiness in 2019?
+ggplot(happiness_2019, aes(x = Country.or.region,
+                           y = Overall.rank,
+                           color = Freedom.to.make.life.choices)) +
+  #facet_grid(Country.or.region ~ Social.support) +
+  geom_point()
+
+
+#The comparisons in 2019 based on the following criteria:
+dropN <- c("Country.or.region", "Overall.rank", "Score")
+criteria <- names(happiness_2019[!(names(happiness_2019)) %in% dropN])
+criteria
+
+
+# How many Country in the dataset?
+nrow(happiness_2019)
+
+
+#Top 10 of the happiest country in 2019
+happiest <- arrange(happiness_2019, Overall.rank)
+head(happiest$Country.or.region, 10)
+
+# Top 10 of the saddest country in 2019
+saddest <- arrange(happiness_2019, desc(Overall.rank))
+head(saddest$Country.or.region, 10)
+
+
+# The average of the Healthy life expectancy for the saddest country in 2019
+happiness_2019 %>% 
+  summarise(avg = mean(Healthy.life.expectancy
+                       [Overall.rank > (nrow(happiness_2019) - 10)]))
+
+# The average of the Healthy life expectancy for the happiest country in 2019
+happiness_2019 %>% 
+  summarise(avg = mean(Healthy.life.expectancy
+                       [Overall.rank > 10]))
+
+
+
+# Saudis Happiness details in 2019 in comparisons with the happiest country
+SvsF <- filter(happiness_2019, Country.or.region == "Saudi Arabia" | Overall.rank == 1)
+SvsF
+
+
+# In plot
+# Columes to get rid of
+drop <- c("Country.or.region", "Overall.rank", "Score")
+lab <- names(SvsF[!(names(SvsF)) %in% drop]) 
+lab
+
+# Saudi Arabia plot
+Sa <- as.double(unname(filter(SvsF, Country.or.region == "Saudi Arabia")), na.rm = TRUE) 
+Sa <- na.omit(Sa)
+pie(labels = lab, Sa, main = "Saudi Arabia")
+
+
+Sa <- filter(SvsF, Country.or.region == "Saudi Arabia") %>% 
+  as.double() %>% 
+  unname()
+
+Sa <- na.omit(Sa)
+pie(labels = lab, Sa, main = "Saudi Arabia")
+# Finland plot
+Fi <- as.double(unname(filter(SvsF, Overall.rank == 1)), na.rm = TRUE)
+Fi <- na.omit(Fi)
+pie(labels = lab, Fi, main = "Finland")
+
+
+#How happiness' rank in Saudi Arabia changed over the last five years
+sa2015 <- filter(happiness_2015, Country == "Saudi Arabia")
+sa2015
+ 
+sa2016 <- filter(happiness_2016, Country == "Saudi Arabia")
+sa2016
+
+sa2017 <- filter(happiness_2017, Country == "Saudi Arabia")
+sa2017
+
+sa2018 <- filter(happiness_2018, Country.or.region == "Saudi Arabia")
+sa2018
+
+sa2019 <- filter(happiness_2019, Country.or.region == "Saudi Arabia")
+sa2019
+
+years <- c("2015", "2016", "2017", "2018", "2019")
+happinessRate <- c(sa2015$Happiness.Rank, sa2016$Happiness.Rank, sa2017$Happiness.Rank, sa2018$Overall.rank, sa2019$Overall.rank)
+
+
+#Plot
+SaHappinessRate <- data.frame(years,happinessRate)
+ggplot(SaHappinessRate, aes(x = years,
+                            y = happinessRate,
+                            fill = years)) +
+   geom_bar(stat="identity")
+
+
+
+
